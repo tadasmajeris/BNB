9b15a255e2bcea61e50f6fd0f2d7e9995964aa48
Tadas Majeris <tadas.majeris@gmail.com>
2016-10-31 17:44:41 +0000

    DATE PICKER, woohoo. updated /spaces/index view

--- spec/features/creating_space_spec.rb
+++ spec/features/creating_space_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 feature 'creating a new space' do
 	scenario 'providing correct details' do
 		visit('/spaces/new')
-		fill_in :name, with: 'Tadas'
-		fill_in :description, with: 'A new space'
+		fill_in :name, with: 'A new space'
+		fill_in :description, with: 'A big and beautiful new space'
 		fill_in :price_per_night, with: '23'
 		fill_in :available_from, with: '10/11/2016'
 		fill_in :available_to, with: '13/11/2016'
 		expect { click_button('List My Space') }.to change(Space,:count).by(1)
-		expect(page).to have_content('Tadas')
 		expect(page).to have_content('A new space')
+		expect(page).to have_content('A big and beautiful new space')
 		expect(page).to have_content('23')
 		expect(page).to have_content('10/11/2016')
 		expect(page).to have_content('13/11/2016')
@@ -18,12 +18,16 @@ feature 'creating a new space' do
 
 	scenario 'providing incorrect details' do
 		visit('/spaces/new')
-		
+
 		expect{ click_button('List My Space') }.not_to change(Space,:count)
 		expect(page).to have_content('Name must not be blank')
 		expect(page).to have_content('Description must not be blank')
 		expect(page).to have_content('Price must not be blank')
 		expect(page).to have_content('Available from must not be blank')
 		expect(page).to have_content('Available to must not be blank')
+	end
+
+	scenario 'available to date cant be before available from' do
+
 	end
 end
