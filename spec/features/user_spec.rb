f501089752b4faff59a8ef04179a535453ef6ec6
crispinandrews <crispin.andrews@gmail.com>
2016-10-31 16:58:04 +0000

    Failing tests for incorrect password and email added

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -7,4 +7,14 @@ feature 'user can sign up' do
     expect(page).to have_content('Welcome example@mail.com!')
     expect { sign_up }.to change(User, :count).by(1)
   end
+  scenario 'The user and the email given match' do
+    sign_up
+    expect(User.first.email).to eq('example@mail.com')
+  end
+  scenario 'user can not sign up if passwords do not match' do
+    expect {sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
+  end
+  scenario 'user can not sign up with an invalid email' do
+    expect {sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
+  end
 end
