111cd95cf7760708b905f01bd0a5f37be54c605c
crispinandrews <crispin.andrews@gmail.com>
2016-10-31 16:52:10 +0000

    Pass initial sign up test

--- app/app.rb
+++ app/app.rb
@@ -20,6 +20,12 @@ class Bnb < Sinatra::Base
     redirect '/users/new'
   end
 
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+  
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,24 +1,17 @@
 class Bnb < Sinatra::Base
 
   get '/users/new' do
-    @user = User.new
     erb :'/users/new'
 
   end
 
   post '/users' do
-    # @user = User.new
     @user = User.new(email: params[:email],
             password: params[:password],
             password_confirmation: params[:password_confirmation])
+    @user.save
+    session[:user_id] = @user.id
     redirect to '/users/new'
   end
-
-
-    helpers do
-      def current_user
-        @current_user ||= User.get(session[:user_id])
-      end
-    end
 
 end
