bd62c36181b6d13438e5dec32b5cddf439972614
crispinandrews <crispin.andrews@gmail.com>
2016-11-01 10:27:46 +0000

    passing tests for sign in. Failing test for sign out

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User
   attr_reader :password
   attr_accessor :password_confirmation
 
+  validates_confirmation_of :password
+
   property :id, Serial
   property :email, String, format: :email_address, required: true, unique: true
   property :password_digest, Text
@@ -15,6 +17,12 @@ class User
     self.password_digest = BCrypt::Password.create(password)
   end
 
-  validates_confirmation_of :password
-
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
+  end
 end
