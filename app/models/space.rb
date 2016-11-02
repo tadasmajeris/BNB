0d25163c2cee31a56cd4d17464f675f2f0aa61d7
Tadas Majeris <tadas.majeris@gmail.com>
2016-11-02 14:30:57 +0000

    updated methods _to_s to skinnier versions

--- app/models/space.rb
+++ app/models/space.rb
@@ -12,7 +12,7 @@ class Space
 	property :available_to, Date, required: true
 
 	def available_from_to_s
-		if !self.errors.full_messages.include?('Available from must be of type Date') && available_from
+		if available_from.class == Date
 			available_from.strftime("%d/%m/%Y")
 		else
 			available_from
@@ -20,7 +20,7 @@ class Space
 	end
 
 	def available_to_to_s
-		if !self.errors.full_messages.include?('Available to must be of type Date')  && available_to
+		if available_to.class == Date
 			available_to.strftime("%d/%m/%Y")
 		else
 			available_to
